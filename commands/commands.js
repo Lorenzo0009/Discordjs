8a864af7c88aa333a27843dcbc909fa0fce3368e
Lorenzo <matheusfertunani@gmail.com>
2020-10-12 18:17:07 -0300

    Class commands

---
+++ commands/commands.js
@@ -0,0 +1,22 @@
+const commando = require('discord.js-commando');
+const oneLine = require('common-tags').oneLine;
+
+module.exports = class ClassName extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'name',
+            aliases: [],
+            group: 'group',
+            memberName: 'name',
+            description: 'description',
+            details: oneLine`
+                description
+            `,
+            examples: ['example'],
+        })
+    }
+
+    async run(msg) {
+        
+    }
+}
